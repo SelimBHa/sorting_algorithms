86a882280f169dfbb4952eb0ad75612bf110da23
SelimBHa <141486892+SelimBHa@users.noreply.github.com>
2024-03-18 09:11:32 +0000

    sorting_algorithms

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-# 0x1B. C - Sorting algorithms & Big O
 
-
-## Author
-* **Selim ben harzallah**
- - [GitHub](https://github.com/SelimBHa)
-
+> sorting algorithms 
--- sort.h
+++ sort.h
@@ -2,6 +2,10 @@
 #define SORT_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* strucutures */
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -16,19 +20,24 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void bubble_sort(int *array, size_t size);
+
+/* function prototypes*/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-listint_t *swap_node(listint_t *node, listint_t **list);
+void check_list(const listint_t *list);
+void check_array(const int *array, size_t size);
+int *rand_array(int len, int max);
+
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
